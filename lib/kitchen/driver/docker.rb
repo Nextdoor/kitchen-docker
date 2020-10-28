1bc7117a110030464dece3e5ae5d014fb68a0e1c
Matt Wise <768067+diranged@users.noreply.github.com>
2020-10-28 14:13:16 -0700

    Run the apt-get update inline with the apt-get install
    Identical to https://github.com/Nextdoor/kitchen-docker/commit/d63f826a2ddd845bfd580ba37d58b5ace9da20c6

--- lib/kitchen/driver/docker.rb
+++ lib/kitchen/driver/docker.rb
@@ -213,8 +213,7 @@ module Kitchen
           packages = <<-eos
             ENV DEBIAN_FRONTEND noninteractive
             ENV container docker
-            RUN apt-get update
-            RUN apt-get install -y sudo openssh-server curl lsb-release
+            RUN apt-get update && apt-get install -y sudo openssh-server curl lsb-release
           eos
           config[:disable_upstart] ? disable_upstart + packages : packages
         when 'rhel', 'centos', 'oraclelinux', 'amazonlinux'
